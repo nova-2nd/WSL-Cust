7ee7cc22af1a2118c535cd45ea079e7e201476ca
Stefan Nader <stefan@nader.zone>
2022-08-11 16:31:52 +0200

    refactor + added verbose control

--- 0-os-debian/debian-image-creator.sh
+++ 0-os-debian/debian-image-creator.sh
@@ -6,35 +6,144 @@ set -e
 
 BUILDIR=$(pwd)
 PARENTDIR=${PWD##*/}
-TMPDIR=$(mktemp -d)
 
 DIST="bullseye"
 
+STATICBOOTSTRAP=0 # Create initial bootstrap folder and keep it for followup runs
+# STATICBOOTSTRAP Does not work right now - Don't use - keep it zero!
+# Probably the bootstrap cloning (Copy static bootstrap folder) portion ...
+STATICBOOTSTRAPFOLDER=/tmp/$DIST-bootstrap
+KEEPSTATICBOOTSTRAP=0
+# Keep bootstrap folder even when static bootstrap disabled
+ONESHOTBOOTSTRAP=0
+# Static bootstrap anyway, what ever is configured
+
+if [ $STATICBOOTSTRAP -eq 1 ] ; then
+    TMPDIR=$STATICBOOTSTRAPFOLDER
+else
+    TMPDIR=$(mktemp -d)
+    if [ -d $STATICBOOTSTRAPFOLDER ] && [ $KEEPSTATICBOOTSTRAP -eq 0 ]; then
+        echo "########################################"
+        echo "Delete static bootstrap folder - start"
+        echo "----------------------------------------"
+        sudo rm -rf $VERBOSEMINUSV $STATICBOOTSTRAPFOLDER
+        echo "----------------------------------------"
+        echo "Delete static bootstrap folder - done"
+        echo "########################################"
+    fi
+fi
+
+BUILDGZ=1
+BUILDXZ=0
+
+VERBOSE=0
+
+if [ $VERBOSE -eq 1 ]; then
+    VERBOSEMINUSV="-v"
+fi
+
 create_x64_rootfs() {
-    rm -f $BUILDIR/../blobs/install.tar.gz
-    # rm -f $BUILDIR/blobs/install.tar.xz
+    if  [ ! -d $TMPDIR ]; then
+        sudo mkdir -p $TMPDIR
+        ONESHOTBOOTSTRAP=1
+    fi
 	cd $TMPDIR
 
-	sudo cdebootstrap -a "amd64" --exclude=debfoster --include=sudo,locales,ansible,gpg,python3-apt $DIST $DIST http://deb.debian.org/debian
+    if [ $STATICBOOTSTRAP -eq 0 ] || [ $ONESHOTBOOTSTRAP -eq 1 ] ; then
+        echo "########################################"
+        echo "Create bootstrap folder - start"
+        echo "----------------------------------------"
+        sudo cdebootstrap -a "amd64" $VERBOSEMINUSV --exclude=debfoster --include=sudo,locales,ansible,gpg,python3-apt $DIST $DIST http://deb.debian.org/debian
+        echo "----------------------------------------"
+        echo "Create bootstrap folder - done"
+        echo "########################################"
+    fi
 
-    sudo cp -r $BUILDIR/inject_files/pre-chroot/* $TMPDIR/$DIST
+    if [ $STATICBOOTSTRAP -eq 1 ]; then
+        NEWTMP=$(mktemp -d)
+        echo "########################################"
+        echo "Copy static bootstrap folder - start"
+        echo "----------------------------------------"
+        sudo cp -ar $VERBOSEMINUSV $TMPDIR/* $NEWTMP
+        echo "----------------------------------------"
+        echo "Copy static bootstrap folder - done"
+        echo "########################################"
+        TMPDIR=$NEWTMP
 
+    fi
+
+    echo "########################################"
+    echo "Injecting pre-chroot files - start"
+    echo "----------------------------------------"
+    sudo cp -ar $VERBOSEMINUSV $BUILDIR/inject_files/pre-chroot/* $TMPDIR/$DIST
+    echo "----------------------------------------"
+    echo "Injecting pre-chroot files - done"
+    echo "########################################"
+
+    echo "########################################"
+    echo "Execute chroot script - start"
+    echo "----------------------------------------"
     sudo chroot $DIST /bin/bash -s < $BUILDIR/dist_files/chroot-tasks.sh
+    echo "----------------------------------------"
+    echo "Execute chroot script - done"
+    echo "########################################"
 
-    sudo rm -f $DIST/etc/resolv.conf
-    sudo rm -f $DIST/etc/hosts
-    sudo cp -r $BUILDIR/inject_files/post-chroot/* $TMPDIR/$DIST
+    echo "########################################"
+    echo "Remove chroot files - start"
+    echo "----------------------------------------"
+    sudo rm -f $VERBOSEMINUSV $DIST/etc/resolv.conf
+    sudo rm -f $VERBOSEMINUSV $DIST/etc/hosts
+    echo "----------------------------------------"
+    echo "Remove chroot files - done"
+    echo "########################################"
 
-    sudo tar -xvf /tmp/opt_distrod.tar.gz -C $DIST/opt/distrod
+    echo "########################################"
+    echo "Injecting post-chroot files - start"
+    echo "----------------------------------------"
+    sudo cp -ar $VERBOSEMINUSV $BUILDIR/inject_files/post-chroot/* $TMPDIR/$DIST
+    echo "----------------------------------------"
+    echo "Injecting post-chroot files - done"
+    echo "########################################"
+
+    echo "########################################"
+    echo "Injecting distrod - start"
+    echo "----------------------------------------"
+    sudo tar $VERBOSEMINUSV -xf $BUILDIR/../blobs/opt_distrod.tar.gz -C $TMPDIR/$DIST/opt/distrod
+    echo "----------------------------------------"
+    echo "Injecting distrod - done"
+    echo "########################################"
 
 	cd $DIST
-    sudo tar --ignore-failed-read -czvf $BUILDIR/../blobs/install.tar.gz *
-	# sudo tar --ignore-failed-read -cJvf $BUILDIR/../blobs/install.tar.xz *
-	# mv $TMPDIR/install.tar.gz $BUILDIR/blobs
-    # mv $TMPDIR/install.tar.xz $BUILDIR/blobs
-    sudo rm -rf $TMPDIR
+
+    if [ $BUILDGZ -eq 1 ]; then
+        echo "########################################"
+        echo "Build gz rootfs - start"
+        echo "----------------------------------------"
+        sudo tar --ignore-failed-read $VERBOSEMINUSV -czf $BUILDIR/../blobs/$DIST.tar.gz *
+        echo "----------------------------------------"
+        echo "Build gz rootfs - done"
+        echo "########################################"
+    fi
+    if [ $BUILDXZ -eq 1 ]; then
+        echo "########################################"
+        echo "Build xz rootfs - start"
+        echo "----------------------------------------"
+	    sudo tar --ignore-failed-read $VERBOSEMINUSV -cJf $BUILDIR/../blobs/$DIST.tar.xz *
+        echo "----------------------------------------"
+        echo "Build xz rootfs - done"
+        echo "########################################"
+    fi
+
+    echo "########################################"
+    echo "Remove bootstrap folder - start"
+    echo "----------------------------------------"
+    sudo rm -rf $VERBOSEMINUSV $TMPDIR
 	cd $BUILDIR
+    echo "----------------------------------------"
+    echo "Remove bootstrap folder - done"
+    echo "########################################"
 }
+
 
 if [ -f .root-identifier ]; then
     if [ $(cat .root-identifier) == $PARENTDIR ]; then
